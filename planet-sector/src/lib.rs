2cfdff7b635f2b04b1e4e82270c6600ea4695b5d
Kaden Cyster <kadencyster@gmail.com>
2026-01-15 22:01:23 -0800

    Restarting to improve comprehension

--- planet-sector/src/lib.rs
+++
@@ -1,9 +0,0 @@
-// lib.rs (for reference only)
-
-use nalgebra::Vector3;
-
-pub fn random_points_on_sphere(n: usize) -> Vec<Vector3<f64>> { /* ... */ }
-
-pub fn spherical_voronoi(
-    points: &[Vector3<f64>]
-) -> Vec<Vec<Vector3<f64>>> { /* ... */ }
